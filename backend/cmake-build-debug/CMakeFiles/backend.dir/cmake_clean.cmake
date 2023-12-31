eea330ec3f1c4fc16a81217798ff9cca4ade60d8
Rezgar01m <rezgar.sasr@gmail.com>
2024-01-01 00:23:48 +0330

    boost added

---
+++ backend/cmake-build-debug/CMakeFiles/backend.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/backend.dir/main.cpp.obj"
+  "CMakeFiles/backend.dir/main.cpp.obj.d"
+  "backend.exe"
+  "backend.exe.manifest"
+  "backend.pdb"
+  "libbackend.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/backend.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
--- backend/main.cpp
+++ backend/main.cpp
@@ -1,6 +1,9 @@
 #include <iostream>
-
+#include "../../lib/SQLiteCpp-master/sqlite3/sqlite3.h"
+#include "../../lib/crow_all.h"
+#include "../../lib/boost/process.hpp"
 int main() {
+    sqlite3 *db;
     std::cout << "Hello,!" << std::endl;
     return 0;
 }
