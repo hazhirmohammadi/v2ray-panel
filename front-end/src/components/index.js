d4c032fbb391cf6efe86f474dd7b8c788a202da5
hazhirmohammadi <hazhir6690@gmail.com>
2024-01-01 10:13:27 +0330

    Feat: implementation structure of the components

---
+++ front-end/src/components/index.js
@@ -0,0 +1,13 @@
+import AddForm from "./AddForm.jsx";
+import Header from "./Header.jsx";
+import Login from "./Login.jsx";
+import UserCard from "./UserCard.jsx";
+import StatusBar from "./StatusBar.jsx";
+
+export {
+   AddForm,
+    Header,
+    Login,
+    UserCard,
+    StatusBar,
+};
