3a9f1c3ca70085b3f7ba0d3b797ef9fcb30bdf49
Rezgar01m <rezgar.sasr@gmail.com>
2024-01-02 02:07:19 +0330

    added Client manger

---
+++ backend/api/ClientManger.h
@@ -0,0 +1,16 @@
+//
+// Created by SoheilSystem on 1/2/2024.
+//
+
+#ifndef BACKEND_CLIENTMANGER_H
+#define BACKEND_CLIENTMANGER_H
+
+
+
+class ClientManger {
+
+};
+
+
+
+#endif //BACKEND_CLIENTMANGER_H
