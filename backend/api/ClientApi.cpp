9168c614adc5d0edf09bd8fe6cdb309ab23eb5fb
Rezgar01m <rezgar.sasr@gmail.com>
2024-01-05 01:34:11 +0330

    added cleient api to retrive clients

---
+++ backend/api/ClientApi.cpp
@@ -0,0 +1,5 @@
+//
+// Created by SoheilSystem on 1/5/2024.
+//
+
+#include "ClientApi.h"
---
+++ backend/api/ClientApi.h
@@ -0,0 +1,16 @@
+//
+// Created by SoheilSystem on 1/5/2024.
+//
+
+#ifndef BACKEND_CLIENTAPI_H
+#define BACKEND_CLIENTAPI_H
+
+
+
+class ClientApi {
+
+};
+
+
+
+#endif //BACKEND_CLIENTAPI_H
