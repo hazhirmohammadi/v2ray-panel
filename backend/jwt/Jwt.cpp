591c1498b0f95ec8e8dc0dbb4e54a1410e7963cd
Rezgar01m <rezgar.sasr@gmail.com>
2024-01-07 00:41:11 +0330

    added Jwt to generate tokens without externl library

---
+++ backend/jwt/Jwt.cpp
@@ -0,0 +1,5 @@
+//
+// Created by SoheilSystem on 1/7/2024.
+//
+
+#include "Jwt.h"
---
+++ backend/jwt/Jwt.h
@@ -0,0 +1,16 @@
+//
+// Created by SoheilSystem on 1/7/2024.
+//
+
+#ifndef BACKEND_JWT_H
+#define BACKEND_JWT_H
+
+
+
+class Jwt {
+
+};
+
+
+
+#endif //BACKEND_JWT_H
