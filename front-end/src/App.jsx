b273b4402a6c4d055d9b3a23ebb26242a5afc1a8
hazhirmohammadi <hazhir6690@gmail.com>
2024-01-09 09:42:43 +0330

    feat: useing Toasify in project

--- front-end/src/App.jsx
+++ front-end/src/App.jsx
@@ -6,18 +6,52 @@ import useModalStore from "./Store.js";
 import {AddForm, Login, UserCard} from "./components/index.js";
 import {userCard} from "./constants.js";
 import useLoginStore from "./LoginStore.js";
+import {ToastContainer, toast} from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+import Toast from "./utils/Toast.js";
+import {useEffect} from "react";
+
 
 function App() {
    const {isOpen, openModal, closeModal, dataView, modalComponent} = useModalStore();
    const {isLoggedIn} = useLoginStore();
+//toast
+
+   if (isLoggedIn === true) {
+      let ee = {
+         type: "success",
+         mas: "Login success"
+      }
+      Toast(ee)
+   }
+
+   let ff = {
+      type: "success",
+      mas: "hii"
+   }
 
 
    return (
        <>
-          {isLoggedIn ? <main className={`${Style.paddingX} ${Style.maximum} pt-4 h-screen`}>
+          {isLoggedIn ? <main className={`${Style.paddingX} ${Style.maximum} pt-4  relative  h-screen`}>
+                 <ToastContainer
+                     style={{
+                        borderRadius: "15% ",
+                        paddingLeft: "55px",
+                        paddingTop: "20px",
+                        marginLeft: "auto",
+                        marginRight: "auto"
+                     }}
+                 />
+
                  <div className="flex flex-col">
                     <Header/>
                  </div>
+
+                 <button onClick={() => {
+                    Toast(ff)
+                 }}>ddd88
+                 </button>
                  {/*Modal*/}
                  <div>
                     <div>
@@ -40,7 +74,9 @@ function App() {
                         />
                     ))}
                  </div>
+
               </main>
+
               :
 
               <div className={`${Style.paddingX} ${Style.maximum} pt-4 h-screen`}>
