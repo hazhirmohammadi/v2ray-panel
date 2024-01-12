afeb4dbdf37980368f9a31bf7518f4a356a93a1e
hazhirmohammadi <hazhir6690@gmail.com>
2024-01-12 11:10:30 +0330

    feat: create store for fetching users

--- front-end/src/Style.js
+++ front-end/src/Style.js
@@ -3,31 +3,6 @@ export const Style = {
    maximum: "max-w-[450px]"
 }
 
-// let data = [];
-// const x = () => {
-//    let a = 1;
-//    for (a; a <= 100; a++) {
-//       const randomInteger = Math.floor(Math.random() * 10000000000000000) + 1;
-//       data.push(randomInteger)
-//       console.log(randomInteger);
-//    }
-// }
-// x()
-// console.log(data)
-// setInterval(()=>{
-//    function generateRandomId(length) {
-//       const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
-//       let randomId = '';
-//
-//       for (let i = 0; i < length; i++) {
-//          const randomIndex = Math.floor(Math.random() * characters.length);
-//          randomId += characters[randomIndex];
-//       }
-//
-//       return randomId;
-//    }
-//    const randomId = generateRandomId(12);
-//    console.log(randomId);
-// },7000)
+
 
 
