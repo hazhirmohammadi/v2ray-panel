8da4822216228afd668a90de7a81629317d26f6b
hazhirmohammadi <hazhir6690@gmail.com>
2024-01-23 12:19:09 +0330

    feat: added customize progress bar

--- front-end/src/common/progress.jsx
+++ front-end/src/common/progress.jsx
@@ -1,9 +1,15 @@
-import React from 'react';
-
-const Progress = () => {
+const Progress = (time) => {
+   //user time
+   const width = time.time / time.max*  100
+   console.log(width)
+   let style = {
+      width: `${width}%`
+   }
    return (
-       <div>
-          
+       <div className={`rounded-2xl bg-[#313131] flex  w-[100%] my-2`}>
+          <div style={style} className={`py-[2px] rounded-2xl bg-[#2FE48D] shadow-emerald-500  shadow-lg`}>
+             <div className="py-[2px] rounded-2xl w-[15px] bg-white ml-auto mr-1 "></div>
+          </div>
        </div>
    );
 };
