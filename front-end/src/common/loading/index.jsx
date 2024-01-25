2b72fd81e359d1f50966cf85fc97e889c22e6471
hazhirmohammadi <hazhir6690@gmail.com>
2024-01-25 18:33:22 +0330

    Style: completed Loading  animation

--- front-end/src/common/loading/index.jsx
+++ front-end/src/common/loading/index.jsx
@@ -4,16 +4,20 @@ import Success from "./Success.jsx";
 import {useEffect, useState} from "react";
 
 const Loading = ({status}) => {
-   const [display, setDisplay] = useState(true)
+   const [display, setDisplay] = useState(true);
+   const [hidden, setHidden] = useState("flex")
    if (status === "success" || status === "error") {
       setInterval(() => {
          setDisplay(false)
+         setInterval(() => {
+            setHidden("hidden")
+         }, [600])
       }, [4000])
    } else {
       console.log("End")
    }
    return (
-       <div className={`w-full ${display ? "flex" : "hidden"}  justify-center absolute pt-4`}>
+       <div className={`w-full ${display ? "flex" : `${hidden} ReversLoading`}   justify-center absolute pt-4`}>
           {status === "padding" ?
               <div
                   className="loading-animate border border-blue-900 min-w-[100px] px-3 py-2 rounded-full absolute z-50  bg-[#262626] shadow-2xl shadow-blue-400">
